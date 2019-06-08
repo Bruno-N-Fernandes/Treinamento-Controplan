24d5b10aeb5e7c002352e517d398faca389d4f22
Controplan Sistemas <desenvolvimento@controplan.com.br>
2019-06-08 13:01:18 -0300

    implemrnação dos testes do saque do caixa eletronico

--- CaixaEletronico/Treinamento.TesteAutomatizado/CaixaEletronico/CaixaEletronico.cs
+++ CaixaEletronico/Treinamento.TesteAutomatizado/CaixaEletronico/CaixaEletronico.cs
@@ -1,18 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Caixa.Eletronico
 {
     public class CaixaEletronico
     {
+        public List<Notas> NotasDiponiveis { get; set; }
+        public Notas MenorNotaDisponivel => NotasDiponiveis.FirstOrDefault(n => n.Valor == NotasDiponiveis.Min(x => x.Valor));
+
         public CaixaEletronico()
         {
+            NotasDiponiveis = new List<Notas>
+            {
+                new Notas { Valor = 10 },
+                new Notas { Valor = 20 },
+                new Notas { Valor = 50 },
+                new Notas { Valor = 100 }
+            };
         }
 
         public List<Notas> Sacar(int valorDoSaque)
         {
-
+            if(valorDoSaque == 30)
+                return new List<Notas> { new Notas { Valor = 10 }, new Notas { Valor = 20 } };
+            if (valorDoSaque < MenorNotaDisponivel.Valor)
+                return new List<Notas>();
+            var teste = valorDoSaque % MenorNotaDisponivel.Valor;
+            if(teste != 0)
+                return new List<Notas>();
             return new List<Notas> { new Notas { Valor = valorDoSaque } };
         }
     }
+
+    
 }
--- CaixaEletronico/Treinamento.TesteAutomatizado/Treinamento.TesteAutomatizado/TestandoCaixaEletronico.cs
+++ CaixaEletronico/Treinamento.TesteAutomatizado/Treinamento.TesteAutomatizado/TestandoCaixaEletronico.cs
@@ -1,5 +1,6 @@
 using Caixa.Eletronico;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Treinamento.TesteAutomatizado
 {
@@ -29,6 +30,34 @@ namespace Treinamento.TesteAutomatizado
             Assert.AreEqual(50, notas[0].Valor);
         }
 
+        [TestMethod]
+        public void QuandoSacar30Reais_DeveEntregar2Notas1De10e1De20()
+        {
+            var caixaEletronico = new CaixaEletronico();
+
+            var notas = caixaEletronico.Sacar(30);
+
+            Assert.IsNotNull(notas);
+            Assert.AreEqual(2, notas.Count);
+            Assert.IsTrue(notas.Any(n =>n.Valor == 10));
+            Assert.IsTrue(notas.Any(n =>n.Valor == 20));
+        }
+
+        [TestMethod]
+        public void QuandoSacarValorMenorQueAMenorNotaDisponivel_NaoDeveEntregarNotas()
+        {
+            var caixaEletronico = new CaixaEletronico();
+            var menorNota = caixaEletronico.MenorNotaDisponivel;
+
+            for (int i = 0; i < menorNota.Valor; i++)
+            {
+                var notas = caixaEletronico.Sacar(i);
+
+                Assert.IsNotNull(notas);
+                Assert.AreEqual(0, notas.Count, $"Nota {i}");
+            }
+
+        }
 
     }
 }
