e9c0ad627686bb5146c99f4e548a26f5e2567aa7
Douglas Silva <douglas.silva@controplan.com.br>
2019-06-08 16:43:39 -0300

    Teste Controplan (Douglas, Renato e Samuel)

--- CaixaEletronico/Treinamento.TesteAutomatizado/CaixaEletronico/CaixaEletronico.cs
+++ CaixaEletronico/Treinamento.TesteAutomatizado/CaixaEletronico/CaixaEletronico.cs
@@ -22,15 +22,60 @@ namespace Caixa.Eletronico
 
         public List<Nota> Sacar(int valorDoSaque)
         {
-            if(valorDoSaque == 30)
-                return new List<Nota> { new Nota { Valor = 10 }, new Nota { Valor = 20 } };
+            switch (valorDoSaque)
+            {
+                case 10:
+                    return new List<Nota> { new Nota { Valor = valorDoSaque } };
+                case 20:
+                    return new List<Nota> { new Nota { Valor = valorDoSaque } };
+                case 50:
+                    return new List<Nota> { new Nota { Valor = valorDoSaque } };
+                case 100:
+                    return new List<Nota> { new Nota { Valor = valorDoSaque } };
+                default:
+                    return VerificaRetornoNota(valorDoSaque);                    
+            }             
+            
+        }
+
+        private List<Nota> VerificaRetornoNota(int valorDoSaque)
+        {            
             if (valorDoSaque < MenorNotaDisponivel.Valor)
                 return new List<Nota>();
             var teste = valorDoSaque % MenorNotaDisponivel.Valor;
-            if(teste != 0)
+            if (teste != 0)
                 return new List<Nota>();
-            return new List<Nota> { new Nota { Valor = valorDoSaque } };
+
+            List <Nota> listaNotaAux = NotasDiponiveis.Where(x => x.Valor < valorDoSaque).ToList();
+            int vlSaque = 0;
+            List<Nota> notaRetorno = new List<Nota>();
+            while (vlSaque != valorDoSaque)
+            {
+                Nota ntAux = new Nota();
+                if (vlSaque == 0)
+                {
+                    vlSaque = listaNotaAux.LastOrDefault().Valor;
+                    ntAux.Valor = vlSaque;
+                    notaRetorno.Add(ntAux);
+                }
+                else
+                {
+                    int vlSaqueAux = listaNotaAux.LastOrDefault(x => x.Valor <= (valorDoSaque - vlSaque)).Valor;
+                    vlSaque += vlSaqueAux;
+                    ntAux.Valor = vlSaqueAux;
+                    notaRetorno.Add(ntAux);
+                }
+            }
+           
+            //if (valorDoSaque == 30)
+              //  return new List<Nota> { new Nota { Valor = 10 }, new Nota { Valor = 20 } };
+
+            return notaRetorno;
+
+
         }
+
+
     }
 
     
